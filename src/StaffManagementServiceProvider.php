cba10d1e03c6ac4c3aafa1c5ada76d0f06827979
Beau Beauchamp, WebTigers <beau@webtigers.com>
2021-02-09 16:55:37 -0500

    Load Migrations from Package

--- src/StaffManagementServiceProvider.php
+++ src/StaffManagementServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tipoff\StaffManagement;
 
 use Spatie\LaravelPackageTools\Package;
@@ -8,6 +10,12 @@ use Tipoff\StaffManagement\Commands\StaffManagementCommand;
 
 class StaffManagementServiceProvider extends PackageServiceProvider
 {
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        parent::boot();
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
@@ -17,9 +25,6 @@ class StaffManagementServiceProvider extends PackageServiceProvider
          */
         $package
             ->name('staff-management')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_staff_management_table')
-            ->hasCommand(StaffManagementCommand::class);
+            ->hasConfigFile();
     }
 }
